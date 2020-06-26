78c619611539f7cfba56b517f7cd2d8d81d94bb1
Caroline <carolineehana@gmail.com>
2020-06-26 22:11:22 +0000

    simplfied  readmore function.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,77 +13,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomFunFacts() {
-  const FunFacts =
-      [, 'My zodiac sign is a Leo', 'I was in a PBS documentary', 'Ive audtioned for jeporady',
-      'My friends and I made own langauge', 'I remember song lyrics more than my own life',
-      'Im terrible at spelling', 'I can sleep while standing', 
-      'I can put my foot behind my head', 'I can touch my nose with my tongue'];
 
-  // Pick a random greeting.
-  const facts = FunFacts[Math.floor(Math.random() * FunFacts.length)];
-
-  // Add it to the page.
-  const factsContainer = document.getElementById('facts-container');
-  factsContainer.innerText = facts;
-}
-
-// adds a collapsible button 
-function ReadMore() {
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("readmorebtn");
+// adds a collapsible button for each blog post
+// shows and hides blog text based on user action 
+function ReadMore(moreId, readmorebtnId) {
+  var moreText = document.getElementById(moreId);
+  var buttonText = document.getElementById(readmorebtnId);
 
    if (moreText.style.display === "inline") {
-    btnText.innerHTML = "+ READ MORE"; 
+    buttonText.innerHTML = "+ READ MORE"; 
     moreText.style.display = "none";
   } else {
-    btnText.innerHTML = "- READ LESS";
-    moreText.style.display = "inline";
-    
-  }
-}
-
-function ReadMore1() {
-  var moreText = document.getElementById("more1");
-  var btnText = document.getElementById("readmorebtn1");
-
-   if (moreText.style.display === "inline") {
-    btnText.innerHTML = "+ READ MORE"; 
-    moreText.style.display = "none";
-  } else {
-    btnText.innerHTML = "- READ LESS";
-    moreText.style.display = "inline";
-    
-  }
-}
-
-function ReadMore2() {
-  var moreText = document.getElementById("more2");
-  var btnText = document.getElementById("readmorebtn2");
-
-   if (moreText.style.display === "inline") {
-    btnText.innerHTML = "+ READ MORE"; 
-    moreText.style.display = "none";
-  } else {
-    btnText.innerHTML = "- READ LESS";
-    moreText.style.display = "inline";
-    
-  }
-}
-  
-
-function ReadMore3() {
-  var moreText = document.getElementById("more3");
-  var btnText = document.getElementById("readmorebtn3");
-
-   if (moreText.style.display === "inline") {
-    btnText.innerHTML = "+ READ MORE"; 
-    moreText.style.display = "none";
-  } else {
-    btnText.innerHTML = "- READ LESS";
+    buttonText.innerHTML = "- READ LESS";
     moreText.style.display = "inline";
     
   }
