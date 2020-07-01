fcb33aa9704b4827e8e79a19041ba148b16e2fd2
Caroline <carolineehana@gmail.com>
2020-07-01 05:53:22 +0000

    Add datastore function to save comments.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,13 +14,16 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +37,20 @@ public class DataServlet extends HttpServlet {
     
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+       Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity entity : results.asIterable()) {
+      long id = entity.getKey().getId();
+      long timestamp = (long) entity.getProperty("timestamp");
+      String text = (String) entity.getProperty("comment");
+     
+     
+      comments.add(text);
+    }
+
     // convert to JSON
     Gson gson = new Gson();
     // Send the JSON as the response
@@ -47,13 +64,11 @@ public class DataServlet extends HttpServlet {
     // Get the input from the form.
     String comment = getUserComment(request, "text-input", "");
 
-    comments.add(comment);
-
-    Entity TextEntity = new Entity("Comment");
-    TextEntity.setProperty("comment", comment);
+    Entity CommentEntity = new Entity("Comment");
+    CommentEntity.setProperty("comment", comment);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.put(TextEntity);
+    datastore.put(CommentEntity);
 
     response.sendRedirect("/main_ENG.html");
 
@@ -61,7 +76,7 @@ public class DataServlet extends HttpServlet {
 
 
   private String getUserComment(HttpServletRequest request, String name, String DefaultValue) {
-    String text = request.getParameter(name);
+    String text = request.getParameter("text-input");
     
     if (text == null) {
       System.err.println("There is no input text");
